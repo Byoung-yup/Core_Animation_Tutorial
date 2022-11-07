a1fd33ce212805fe27ca5dcea0c7e7a7abee84b6
Byoung-yup <20170926@vision.hoseo.edu>
2022-11-07 22:46:20 +0900

    first commit

--- Core_Animation_Tutorial/ViewController.swift
+++ Core_Animation_Tutorial/ViewController.swift
@@ -7,13 +7,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CAAnimationDelegate {
+    
+    let gradient = CAGradientLayer()
+    
+    var gradientSet = [[CGColor]]()
+    
+    var currentGradient: Int = 0
+    
+    let colorOne = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1).cgColor
+    let colorTwo = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).cgColor
+    let colorThree = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1).cgColor
 
+    @IBOutlet weak var countdownProgressBar: CountdownProgressBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
-
-
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        createGradientView()
+        countdownProgressBar.startCoundown(duration: 10, showPulse: false)
+    }
+    
+    @objc func handleTap() {
+        print("Tapped")
+            
+        countdownProgressBar.startCoundown(duration: 10, showPulse: true)
+    }
+    
+    func createGradientView() {
+        
+        // overlaps the colors and make it 3 sets of colors
+        gradientSet.append([colorOne, colorTwo])
+        gradientSet.append([colorTwo, colorThree])
+        gradientSet.append([colorThree, colorOne])
+        
+        // set the gradient size to be the entire screen
+        gradient.frame = self.view.bounds
+        gradient.colors = gradientSet[currentGradient]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        gradient.drawsAsynchronously = true
+        
+        self.view.layer.insertSublayer(gradient, at: 0)
+        
+        animateGradient()
+    }
+    
+    func animateGradient() {
+        
+        // cycle through all the colors, feel free to add more to the set
+        if currentGradient < gradientSet.count - 1 {
+            currentGradient += 1
+        } else {
+            currentGradient = 0
+        }
+        
+        // animate over 3 seconds
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "colors")
+        gradientChangeAnimation.duration = 3.0
+        gradientChangeAnimation.toValue = gradientSet[currentGradient]
+        gradientChangeAnimation.fillMode = CAMediaTimingFillMode.forwards
+        gradientChangeAnimation.isRemovedOnCompletion = false
+        gradientChangeAnimation.delegate = self
+        gradient.add(gradientChangeAnimation, forKey: "gradientChangeAnimation")
+    }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        
+        // if our gradient animation ended animating, restart the animation by changing the color set
+        if flag {
+            gradient.colors = gradientSet[currentGradient]
+            animateGradient()
+        }
+    }
 }
 
